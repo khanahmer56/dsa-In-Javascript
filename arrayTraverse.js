195446bc847a966e3fd6bb7c45f9ec92f24d6f95
Ahmer khan <khanahmer56@gmail.com>
2024-08-26 22:16:29 +0530

    delete element at particular position

--- arrayTraverse.js
+++ arrayTraverse.js
@@ -8,4 +8,14 @@ for (let i = data.length; i > position; i--) {
 
 data[position] = newEl;
 
-console.log(data);
+// Deleting a element from an array at particular position
+
+let data2 = [20, 10, 15, 34, 25, 54];
+let pos = 2;
+
+for (let i = pos; i < data2.length; i++) {
+  data2[i] = data2[i + 1];
+}
+
+data2.length = data2.length - 1;
+console.log(data2);
