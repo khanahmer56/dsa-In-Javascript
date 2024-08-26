941440aef6c884c6bf7e93e1e8697e8cb36c43ef
Ahmer khan <khanahmer56@gmail.com>
2024-08-26 21:49:54 +0530

    solve

--- arrayTraverse.js
+++ arrayTraverse.js
@@ -1,1 +1,11 @@
-console.log(1 + 2);
+let data = [20, 10, 15, 34, 25, 54];
+let position = 3;
+let newEl = 22;
+
+for (let i = data.length; i > position; i--) {
+  data[i] = data[i - 1];
+}
+
+data[position] = newEl;
+
+console.log(data);
